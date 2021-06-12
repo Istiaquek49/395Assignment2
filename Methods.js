a30e63aadde26a9caf86cbd05f58c5effc8e308f
Istiaque Khan <Istiaque.Khan49@myhunter.cuny.edu>
2021-06-11 23:07:16 -0400

    Added object keys and values

--- Methods.js
+++ Methods.js
@@ -1,8 +1,31 @@
 //forEach()
-Array.prototype.myEach = function (callback) {
-    for (let i = 0; i < this.length; i++) {
+Array.prototype.myEach = function (callback)
+{
+    for (let i = 0; i < this.length; i++)
+    {
         callback(this[i], i, this);
     }
     return;  
 }
 
+//Object.keys()
+function grabKeys(object)
+{
+    let arr = []; //empty array
+    for (element in obj)
+    {
+        arr.push(element); 
+    }
+    return arr;//returns the keys of the key value pairs
+}
+
+//object.values()
+function grabValues(callback)
+{
+    let newArray = []//empty arry
+    for (let items in callback)
+    {
+      newArray.push(callback[items]);
+    }
+    return newArray;//returns the values of the key value pairs 
+};
